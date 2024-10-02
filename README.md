df5f2d4f2d64e948a3ec2f6162269627d713ab8b
Matias Caputti <matias.caputti@gmail.com>
2024-10-02 09:38:22 -0300

    Add extension and host example

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # JupyterLab Notebook messages extension
 
-A JupyterLab extension to read messages via JavaScript API.
+A JupyterLab extension to communicate between a host page and a Jupyter instance running in an iframe.
+
+## Valid messages
+
+### Inbound messages
+
+#### Go to certain cell by index
+
+- type: **go-to-cell-index**
+- targetCellIndex: [number]
+
+```javascript
+'{"type": "go-to-cell-index", "targetCellIndex": 0}'
+```
+
+#### Go to the first cell that matches a metadata key and value
+
+- type: **go-to-cell-metadata**
+- targetMetadataKey: [string]
+- targetMetadataValue: [string]
+
+```javascript
+'{"type": "go-to-cell-metadata", "targetMetadataKey": "custom_key", "targetMetadataValue": "custom_value"}'
+```
+
+#### Toggle theme
+
+If the current theme is `JupyterLab Light`, it switches the theme to `JupyterLab Dark`, and vice versa. 
+
+- type: **toggle-theme**
+
+```javascript
+'{"type": "toggle-theme"}'
+```
+
+### Outbound messages
+
+TODO
 
 ## Requirements
 
@@ -41,6 +78,15 @@ jupyter lab
 
 With the watch command running, every saved change will immediately be built locally and available in your running JupyterLab. Refresh JupyterLab to load the change in your browser (you may need to wait several seconds for the extension to be rebuilt).
 
+### Example frontend host
+
+Run the example frontend host with an iframe containing a Jupyter Notebook:
+
+```bash
+cd example/
+python -m http.server 8000
+```
+
 ### Publishing
 
 * Update version in `package.json`
@@ -59,4 +105,25 @@ cd /Users/{USER}/.venv/share/jupyter/labextensions/jupyterlab-notebook-messages
 
 ```bash
 pip install jupyterlab-notebook-messages
+```
+
+## Troubleshooting
+
+> Refused to frame because an ancestor violates the following Content Security Policy directive: "frame-ancestors 'self'".
+
+Create a default config file:
+
+```bash
+jupyter notebook --generate-config
+```
+
+then add a new header at the end of the file:
+
+```bash
+# show all Jupyter paths
+jupyter --paths
+
+nano jupyter_notebook_config.d
+
+c.NotebookApp.tornado_settings={'headers': {'Content-Security-Policy': "frame-ancestors self *"}}
 ```
--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,108 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { IThemeManager } from '@jupyterlab/apputils';
+import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
+import { Cell } from '@jupyterlab/cells';
 
 const PLUGIN_ID = 'jupyterlab-notebook-messages:plugin';
-
 const extension: JupyterFrontEndPlugin<void> = {
   id: PLUGIN_ID,
-  description: 'A JupyterLab extension to read messages via JavaScript API.',
+  description: 'A JupyterLab extension to communicate between a host page and a Jupyter instance running in an iframe.',
   autoStart: true,
-  requires: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settings: ISettingRegistry) => {
-    if (app.name === 'JupyterLab') {
-      return;
-    }
+  requires: [INotebookTracker, IThemeManager],
+  activate: (
+    app: JupyterFrontEnd,
+    tracker: INotebookTracker,
+    themeManager: IThemeManager
+  ) => {
+    console.log('[NOTEBOOK MESSAGES EXTENSION] Loaded');
 
-    Promise.all([app.restored, settings.load(PLUGIN_ID)])
-      .then(() => {
-        console.log(`[NOTEBOOK MESSAGES EXTENSION] Loaded`);
+    const goToCellIndex = (targetCellIndex: number) => {
+      const notebookPanel: NotebookPanel | null = tracker.currentWidget;
 
-        setTimeout(() => {
-          console.log("TEST!!!")
-        }, 1000);
-      })
-      .catch(reason => {
-        console.error(
-          `Something went wrong when reading the settings.\n${reason}`
+      if (!notebookPanel) {
+        console.error('No active notebook.');
+        return;
+      }
+
+      const notebook = notebookPanel.content;
+
+      if (targetCellIndex >= 0 && targetCellIndex < notebook.widgets.length) {
+        notebook.activeCellIndex = targetCellIndex;
+        notebook.scrollToItem(targetCellIndex);
+      }
+    };
+
+    const goToCellMetadata = (
+      targetMetadataKey: string,
+      targetMetadataValue: string
+    ) => {
+      const notebookPanel: NotebookPanel | null = tracker.currentWidget;
+
+      if (!notebookPanel) {
+        console.error('No active notebook.');
+        return;
+      }
+
+      const notebook = notebookPanel.content;
+
+      const matchingCellIndex = notebook.widgets.findIndex((cell: Cell) => {
+        const metadata = cell.model.metadata;
+        return (
+          metadata?.[targetMetadataKey] &&
+          metadata?.[targetMetadataKey] === targetMetadataValue
         );
       });
+
+      if (
+        matchingCellIndex >= 0 &&
+        matchingCellIndex < notebook.widgets.length
+      ) {
+        notebook.activeCellIndex = matchingCellIndex;
+        notebook.scrollToItem(matchingCellIndex);
+      }
+    };
+
+    window.addEventListener(
+      'message',
+      event => {
+        // TODO: protect origins and filter valid origin
+        console.log(event.origin);
+
+        console.log('[IFRAME] read message:', event);
+        const messageData = JSON.parse(event.data);
+        console.log('Parsed message: ', messageData);
+
+        if (messageData?.type === 'go-to-cell-index') {
+          goToCellIndex(messageData?.targetCellIndex);
+        }
+
+        if (messageData?.type === 'go-to-cell-metadata') {
+          goToCellMetadata(
+            messageData?.targetMetadataKey,
+            messageData?.targetMetadataValue
+          );
+        }
+
+        if (messageData?.type === 'toggle-theme') {
+          if (themeManager.theme === 'JupyterLab Dark') {
+            themeManager.setTheme('JupyterLab Light');
+          } else {
+            themeManager.setTheme('JupyterLab Dark');
+          }
+        }
+      },
+      false
+    );
+
+    // TODO: outgoing messages management
+    // const notifyHost = (): void => {
+    //   const message = { type: 'from-iframe-to-host', data: 'test' };
+    //   window.parent.postMessage(message, '*');
+    //   console.log('[IFRAME] send message:', message);
+    // };
+    // themeManager.themeChanged.connect(notifyHost);
   }
 };
 
